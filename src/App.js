dee04d533cb7751e963a6d2e913a1db238646b2c
Eryk Mariankowski <dagi12@o2.pl>
2018-08-25 13:33:41 +0200

    form test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import {Alert} from 'react-bootstrap';
+
+const required = (value) => {
+  if (!value.toString().trim().length) {
+    return 'require';
+  }
+};
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -10,9 +21,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form>
+          <Input value="" name="test" validations={[required]} />
+        </Form>
+        <Alert bsStyle="warning">
+          <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
+          good.
+        </Alert>
       </div>
     );
   }
