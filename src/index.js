037e3388dd57cce91e1f9d79793ba1416ce2c7f7
Eryk Mariankowski <dagi12@o2.pl>
2018-08-25 14:37:15 +0200

    ejection, debug works

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// import cssVars from 'css-vars-ponyfill/dist/css-vars-ponyfill.min';
+
+// cssVars();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
